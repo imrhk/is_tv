e5eddc5fe4aa72627ced0fb435014fa6c68e38cf
Rahul Kumar <rahul.cs.mnnit@gmail.com>
2025-11-25 01:51:52 +0530

    fix for https://github.com/imrhk/is_tv/issues/1 macos：all statements inside a switch must be covered by a 'case' or 'default' #1. Upgraded project to be compatible with latest flutter version

--- ios/Classes/IsTVPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface IsTVPlugin : NSObject<FlutterPlugin>
-@end
--- ios/Classes/IsTVPlugin.swift
+++ ios/Classes/IsTVPlugin.swift
@@ -1,19 +1,20 @@
-import Cocoa
-import FlutterMacOS
+import Flutter
+import UIKit
 
 public class IsTVPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "is_tv", binaryMessenger: registrar.messenger)
+    let channel = FlutterMethodChannel(name: "is_tv", binaryMessenger: registrar.messenger())
     let instance = IsTVPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "check":
-      result(false)
-    default:
-      result(FlutterMethodNotImplemented)
-    }
+      if(call.method == "check") {
+        #if os(tvOS)
+            result(true)
+        #else
+            result(false)
+        #endif
+      }
   }
 }
